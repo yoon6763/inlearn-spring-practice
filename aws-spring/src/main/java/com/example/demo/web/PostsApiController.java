6964a4d95ef89a47553584377abceac0e749568c
DESKTOP-TFUUGQ7\8_915_차준혁 <yoon6763@naver.com>
2022-09-21 16:56:49 +0900

    22.09.21

--- aws-spring/src/main/java/com/example/demo/web/PostsApiController.java
+++ aws-spring/src/main/java/com/example/demo/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.example.demo.web;
 
 import com.example.demo.service.posts.PostsService;
+import com.example.demo.web.dto.PostsResponseDto;
 import com.example.demo.web.dto.PostsSaveRequestDto;
+import com.example.demo.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,5 +15,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
--- aws-spring/src/main/java/com/example/demo/web/dto/PostsSaveRequestDto.java
+++ aws-spring/src/main/java/com/example/demo/web/dto/PostsSaveRequestDto.java
@@ -1,4 +1,29 @@
 package com.example.demo.web.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import com.example.demo.domain.posts.Posts;
+
+@Getter
+@NoArgsConstructor
 public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
 }
