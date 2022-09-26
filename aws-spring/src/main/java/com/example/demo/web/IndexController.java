a891615dbf82b4078363c818c935ebed8198e32f
915_랩실컴 <yoon6763@naver.com>
2022-09-26 15:50:49 +0900

    mustache complete

--- aws-spring/src/main/java/com/example/demo/web/IndexController.java
+++ aws-spring/src/main/java/com/example/demo/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.example.demo.web;
 
 import com.example.demo.service.posts.PostsService;
+import com.example.demo.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -13,12 +15,20 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("posts",postsService.findAllDesc());
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
