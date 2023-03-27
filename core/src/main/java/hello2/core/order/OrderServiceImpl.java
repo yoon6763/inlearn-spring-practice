1a939cc9fc8d21a9a92889be6925f6c8bdf15bf1
uknow-home <yoon6763@naver.com>
2023-03-28 00:48:01 +0900

    Lombok

--- core/src/main/java/hello2/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello2/core/order/OrderServiceImpl.java
@@ -3,22 +3,17 @@ package hello2.core.order;
 import hello2.core.discount.DiscountPolicy;
 import hello2.core.member.Member;
 import hello2.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final 붙은 것들 생성자 자동으로 만들어줌
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
