d9dd5330d5c3f7809c07c11b95e895ba61e5ea74
uknow-home <yoon6763@naver.com>
2023-05-02 01:15:26 +0900

    SpringMemberFormControllerV1

--- spring-mvc-1/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ spring-mvc-1/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan
+@ServletComponentScan // 서블렛 자동 등록
 @SpringBootApplication
 public class ServletApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ServletApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServletApplication.class, args);
+    }
 
 
 }
