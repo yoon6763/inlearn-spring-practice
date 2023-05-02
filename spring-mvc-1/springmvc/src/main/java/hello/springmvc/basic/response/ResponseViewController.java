6b83fcb4ef50df87a557c19e2684356e2c2bd817
uknow-mac <yoon6763@naver.com>
2023-05-02 18:01:09 +0900

    HTTP 응답

---
+++ spring-mvc-1/springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,23 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello").addObject("data", "hello!");
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "hello!");
+        return "response/hello"; // 뷰의 논리적 이름이 됨
+        // @RequestBody 가 붙으면 그냥 통 문자열 response/hello 가 감
+    }
+}
