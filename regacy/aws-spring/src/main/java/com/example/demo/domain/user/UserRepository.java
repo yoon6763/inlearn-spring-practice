3d9d8a8348845134a57f356e3db187be3b60a25d
uknow-mac <yoon6763@naver.com>
2023-09-25 16:56:02 +0900

    실전! 스프링 부트와 JPA 활용1

--- regacy/aws-spring/src/main/java/com/example/demo/domain/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.domain.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<Users,Long> {
-    Optional<Users> findByEmail(String email);
-}
