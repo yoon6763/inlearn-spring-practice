ffbe2b2bd1eacbb852d28d0b56e8c07ae83075fd
uknow-mac <yoon6763@naver.com>
2023-11-21 17:25:03 +0900

    JPA NamedQuery

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -9,6 +9,10 @@ import javax.persistence.*;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id
