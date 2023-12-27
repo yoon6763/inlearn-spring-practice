8d235d56bfe1cbe82038083fe9cee21d038e312e
uknow-home <yoon6763@naver.com>
2023-12-28 02:23:10 +0900

    양방향 연관관계와 연관관계의 주인 1- 기본

--- jpa-basic/src/main/java/hellojpa/domain/Team.java
+++ jpa-basic/src/main/java/hellojpa/domain/Team.java
@@ -1,6 +1,8 @@
 package hellojpa.domain;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -12,4 +14,33 @@ public class Team {
 
     private String name;
 
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<Member>();
+
+    public Team() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
 }
