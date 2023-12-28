e79e67c0e35ca8fe938cf34418ea7be2ffdcb148
uknow-mac <yoon6763@naver.com>
2023-12-28 17:06:38 +0900

    상속관계 매핑

---
+++ jpa-basic/src/main/java/hellojpa/domain/Album.java
@@ -0,0 +1,15 @@
+package hellojpa.domain;
+
+import javax.persistence.Entity;
+
+public class Album extends Item {
+    private String artist;
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+}
