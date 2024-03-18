49440fb00c8a60c06bc1c50e9dcfee4cdd046d8b
uknow-home <yoon6763@naver.com>
2024-03-18 18:42:07 +0900

    Template callback pattern

---
+++ spring-advanced/src/main/java/hello/advanced/app/v5/OrderServiceV5.java
@@ -0,0 +1,28 @@
+package hello.advanced.app.v5;
+
+import hello.advanced.trace.callback.TraceCallback;
+import hello.advanced.trace.callback.TraceTemplate;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceV5 {
+
+    private final OrderRepositoryV5 orderRepository;
+    private final TraceTemplate template;
+
+    public OrderServiceV5(OrderRepositoryV5 orderRepository, LogTrace template) {
+        this.orderRepository = orderRepository;
+        this.template = new TraceTemplate(template);
+    }
+
+    public void orderItem(String itemId) {
+        template.execute("OrderService.orderItem()", () -> {
+            orderRepository.save(itemId);
+            return null;
+        });
+    }
+
+}
---
+++ spring-advanced/src/test/java/hello/advanced/trace/strategy/TemplateCallbackTest.java
@@ -0,0 +1,36 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.callback.Callback;
+import hello.advanced.trace.strategy.code.callback.TimeLogTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateCallbackTest {
+
+    @Test
+    void callbackV1() {
+        TimeLogTemplate template = new TimeLogTemplate();
+        template.execute(new Callback() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 실행");
+            }
+        });
+
+        template.execute(new Callback() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+    }
+
+    @Test
+    void callbackV2() {
+        TimeLogTemplate template = new TimeLogTemplate();
+        template.execute(() -> log.info("비즈니스 로직 실행"));
+        template.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+
+}
