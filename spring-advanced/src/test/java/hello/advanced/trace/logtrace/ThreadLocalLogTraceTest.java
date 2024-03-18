97c2ffcfb5bf3c7e48eadf5e4009ab6e5960a8c6
uknow-home <yoon6763@naver.com>
2024-03-18 16:49:26 +0900

    ThreadLocal

---
+++ spring-advanced/src/test/java/hello/advanced/trace/logtrace/ThreadLocalLogTraceTest.java
@@ -0,0 +1,26 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+public class ThreadLocalLogTraceTest {
+
+    ThreadLocalLogTrace trace = new ThreadLocalLogTrace();
+
+    @Test
+    void begin_end_level2() {
+        TraceStatus status1 = trace.begin("hello");
+        TraceStatus status2 = trace.begin("world");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception_level2() {
+        TraceStatus status1 = trace.begin("hello");
+        TraceStatus status2 = trace.begin("world");
+        trace.exception(status2, new IllegalStateException());
+        trace.end(status1);
+    }
+
+}
