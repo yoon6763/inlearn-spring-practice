ebfab1a749adc7116b73f66a9e45be2e9fbefc79
전윤호 Uknow <yoon6763@naver.com>
2024-11-29 00:08:43 +0900

    트랜잭션 V3

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -1,7 +1,6 @@
 package hello.jdbc.repository;
 
 import com.zaxxer.hikari.HikariDataSource;
-import hello.jdbc.connection.ConnectionConst;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,40 +21,42 @@ class MemberRepositoryV1Test {
 
     @BeforeEach
     void beforeEach() {
+        //기본 DriverManager - 항상 새로운 커넥션을 획득
 //        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        //커넥션 풀링
         HikariDataSource dataSource = new HikariDataSource();
         dataSource.setJdbcUrl(URL);
         dataSource.setUsername(USERNAME);
-        dataSource.setPassword(PASSWORD);
+        dataSource.setPoolName(PASSWORD);
         repository = new MemberRepositoryV1(dataSource);
     }
 
     @Test
     void crud() throws SQLException {
-        // save
-        Member member = new Member("memberV1", 10000);
+        //save
+        Member member = new Member("memberV100", 10000);
         repository.save(member);
 
-        // findById
+        //findById
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember={}", findMember);
         assertThat(findMember).isEqualTo(member);
 
-        // update: money 10000 -> 20000
+        //update: money: 10000 -> 20000
         repository.update(member.getMemberId(), 20000);
         Member updatedMember = repository.findById(member.getMemberId());
         assertThat(updatedMember.getMoney()).isEqualTo(20000);
 
-        // delete
+        //delete
         repository.delete(member.getMemberId());
         assertThatThrownBy(() -> repository.findById(member.getMemberId()))
                 .isInstanceOf(NoSuchElementException.class);
 
         try {
             Thread.sleep(1000);
-        } catch (Exception e) {
-
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
-
 }
